1b0a7608442ca32054d67baef8728c759d6ee1cd
Anya Krivosheeva <anyakrivosheeva98@gmail.com>
2026-02-05 19:59:21 +0300

    add redux

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,9 +1,7 @@
 import * as SC from "./styles";
 
-export default function Link({ simple = true, children, ...props }) {
+export default function Link({ children, ...props }) {
     return (
-        simple
-            ? <SC.SimpleLink {...props}>{children}</SC.SimpleLink>
-            : <SC.NavigationLink {...props}>{children}</SC.NavigationLink>
+        <SC.SimpleLink {...props}>{children}</SC.SimpleLink>
     )
 };
--- src/pages/posts/PostsPage.jsx
+++ src/pages/posts/PostsPage.jsx
@@ -1,46 +1,16 @@
 import Posts from "../../components/Posts/Posts";
 import Typo from "../../components/Typo/Typo";
 import Container from "../../components/Сontainer/Container";
-
-export const INITIAL_POSTS = [
-    {
-        id: 1,
-        title: 'Post 1',
-        image: 'https://png.pngtree.com/thumb_back/fh260/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem vitae sapiente accusantium, dolorum provident tempore rerum ex neque aspernatur?',
-    },
-    {
-        id: 2,
-        title: 'Post 2',
-        image: 'https://png.pngtree.com/thumb_back/fh260/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem vitae sapiente accusantium, dolorum provident tempore rerum ex neque aspernatur?',
-    },
-    {
-        id: 3,
-        title: 'Post 3',
-        image: 'https://png.pngtree.com/thumb_back/fh260/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem vitae sapiente accusantium, dolorum provident tempore rerum ex neque aspernatur?',
-    },
-    {
-        id: 4,
-        title: 'Post 4',
-        image: 'https://png.pngtree.com/thumb_back/fh260/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem vitae sapiente accusantium, dolorum provident tempore rerum ex neque aspernatur?',
-    },
-    {
-        id: 5,
-        title: 'Post 5',
-        image: 'https://png.pngtree.com/thumb_back/fh260/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rem vitae sapiente accusantium, dolorum provident tempore rerum ex neque aspernatur?',
-    },
-];
+import { useSelector } from "react-redux";
 
 export default function PostsPage() {
+    const posts = useSelector((state) => state.posts.posts);
+
     return (
         <>
             <Typo>Публикации</Typo>
             <Container>
-                <Posts posts={INITIAL_POSTS} />
+                <Posts posts={posts} />
             </Container>
         </>
     )
