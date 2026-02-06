11f48ccdfbebbff1bdfff7c329f70f590ca18b25
Anya Krivosheeva <anyakrivosheeva98@gmail.com>
2026-02-06 17:14:16 +0300

    add post form

--- src/api/postsAPI.js
+++ src/api/postsAPI.js
@@ -2,7 +2,7 @@
 export const postsAPI = {
     async fetchPosts() {
         try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_sort=id&_order=desc`);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_sort=id&_order=desc&_limit=10`);
             const posts = await response.json();
             return posts;
         } catch (ex) {
